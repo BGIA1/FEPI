9b42f487ca251759acb0a215998673d998c8a09d
Sergio Tinoco <95841694+SerchLawton@users.noreply.github.com>
2025-01-13 13:51:34 -0600

    Add files via upload

--- Proyecto/Applicación/CONAFE/app/build.gradle.kts
+++ Proyecto/Applicación/CONAFE/app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    kotlin("plugin.serialization") version "1.9.23"
 }
 
 android {
@@ -59,4 +61,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(platform("io.github.jan-tennert.supabase:bom:3.0.3"))
+    implementation("io.github.jan-tennert.supabase:postgrest-kt")
+    implementation("io.ktor:ktor-client-android:3.0.3")
 }
